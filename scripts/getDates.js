11b56de686035b706cd037c9558b21a185ca37e3
harold2001 <hcarazasnires@gmail.com>
2024-06-12 01:28:07 -0500

    index.html added and semi-finished

---
+++ scripts/getDates.js
@@ -0,0 +1,5 @@
+const year = new Date().getFullYear()
+
+document.querySelector('#year').textContent = year
+
+document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
