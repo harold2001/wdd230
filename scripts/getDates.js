c086cec79a1281a6e8bbc00a2adf0b7fa20885d1
harold2001 <hcarazasnires@gmail.com>
2024-06-14 22:40:28 -0500

    page finished

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -1,5 +1,5 @@
 const year = new Date().getFullYear()
 
-document.querySelector('#year').textContent = year
+document.querySelector('#year').textContent += year
 
 document.querySelector('#lastModified').textContent = `Last Modification: ${document.lastModified}`
