bff0f2e869e4f1097d7e57ec91d823fb21d765ca
harold2001 <hcarazasnires@gmail.com>
2024-06-22 15:18:10 -0500

    dark mode 50%

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+const darkButton = document.querySelector('#darkBtn');
+const main = document.querySelector('main');
+
+hamButton.addEventListener('click', () => {
+  navigation.classList.toggle('open');
+  hamButton.classList.toggle('open');
+});
+
+darkButton.addEventListener('click', () => {
+  main.classList.toggle('dark');
+});
