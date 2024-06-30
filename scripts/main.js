54dbcb13c6ad6f752f8b7ae17dddc2d8f28c5164
harold2001 <hcarazasnires@gmail.com>
2024-06-30 02:22:09 -0500

    home page edited

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,16 @@ const navigation = document.querySelector('.navigation');
 const darkButton = document.querySelector('#darkBtn');
 const main = document.querySelector('main');
 const nav = document.querySelector('nav');
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+
+if (numVisits !== 0) {
+	visitsDisplay.textContent = numVisits;
+} else {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
 
 hamButton.addEventListener('click', () => {
   navigation.classList.toggle('open');
@@ -13,3 +23,5 @@ darkButton.addEventListener('click', () => {
   main.classList.toggle('dark');
   nav.classList.toggle('dark');
 });
+
+
