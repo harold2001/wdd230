8575d022d4696edba674f9918ad6e39327e64228
harold2001 <hcarazasnires@gmail.com>
2024-07-06 00:32:04 -0500

    form page

--- scripts/main.js
+++ scripts/main.js
@@ -7,9 +7,9 @@ const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
 
 if (numVisits !== 0) {
-	visitsDisplay.textContent = numVisits;
+	visitsDisplay && (visitsDisplay.textContent = numVisits);
 } else {
-	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+	visitsDisplay && (visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`);
 }
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
