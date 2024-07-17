23184387d4e8bc0e707ebb905d74aaf7f3cf6754
harold2001 <hcarazasnires@gmail.com>
2024-07-17 00:45:58 -0500

    JSON data added

---
+++ scripts/links.js
@@ -0,0 +1,9 @@
+const baseURL = 'https://harold2001.github.io/wdd230/';
+const linksURL = 'https://harold2001.github.io/wdd230/data/links.json';
+async function getLinks() {
+  const response = await fetch(linksURL);
+  const data = await response.json();
+  console.log(data);
+}
+
+getLinks();
